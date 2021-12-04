4daadb1c834f325dea0e460569fb4159ebe02f2d
Vibin Joby <vibin2joby@gmail.com>
2021-12-04 16:43:08 -0500

    unused button

--- src/components/AppLoader/stories.tsx
+++ src/components/AppLoader/stories.tsx
@@ -2,6 +2,5 @@ import React from 'react'
 import { storiesOf } from '@storybook/react-native'
 
 import AppLoader from './component'
-import Button from '../Button'
 
 storiesOf('Components', module).add('AppLoader', () => <AppLoader />)
